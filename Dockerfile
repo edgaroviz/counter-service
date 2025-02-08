e9a4800a24a5bf04cc94d96677ca92f64b33901d
Edgar Green <edgar.green@cybereason.com>
2025-02-08 23:58:44 +0200

    final locations and code test

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY . .
+COPY /counter_service_app .
 
 EXPOSE 80
 
--- counter_service_app/counter-service.py
+++ counter_service_app/counter-service.py
@@ -7,18 +7,18 @@ request_counters = {"POST": 0, "GET": 0}
 
 @app.route('/', methods=["POST", "GET"])
 def index():
+    env = os.getenv("env", "not set")
     if request.method == "POST":
         request_counters["POST"] += 1
-        # return f"Our POST counter is: {request_counters['POST']}\n"
     elif request.method == "GET":
         request_counters["GET"] += 1
-        # return f"Our GET counter is: {request_counters['GET']}\n"
-    return f"ur GET counter is: {request_counters['GET']}\nOur POST counter is: {request_counters['POST']}\n"
+    return f"""
+            This is {env}!!!
+            Our GET counter is: {request_counters['GET']}
+            Our POST counter is: {request_counters['POST']}
+            Thanks and dont forget to post once in a while :)
+            """
 
-# @app.route('/stats', methods=["GET"])
-# def stats():
-#     """New route to check request counts"""
-#     return f"Our POST counter is: {request_counters['POST']}, and our GET counter is: {request_counters['GET']}.\n"
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
