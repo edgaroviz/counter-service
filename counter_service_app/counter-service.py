ba4fffe9ebe804edf26bc65f3bb8f5ced7c4bed7
Edgar Green <edgar.green@cybereason.com>
2025-02-08 03:10:48 +0200

    fix application output

--- counter_service_app/counter-service.py
+++ counter_service_app/counter-service.py
@@ -9,15 +9,16 @@ request_counters = {"POST": 0, "GET": 0}
 def index():
     if request.method == "POST":
         request_counters["POST"] += 1
-        return f"Our POST counter is: {request_counters['POST']}\n"
+        # return f"Our POST counter is: {request_counters['POST']}\n"
     elif request.method == "GET":
         request_counters["GET"] += 1
-        return f"Our GET counter is: {request_counters['GET']}\n"
+        # return f"Our GET counter is: {request_counters['GET']}\n"
+    return f"ur GET counter is: {request_counters['GET']}\nOur POST counter is: {request_counters['POST']}\n"
 
-@app.route('/stats', methods=["GET"])
-def stats():
-    """New route to check request counts"""
-    return f"Our POST counter is: {request_counters['POST']}, and our GET counter is: {request_counters['GET']}.\n"
+# @app.route('/stats', methods=["GET"])
+# def stats():
+#     """New route to check request counts"""
+#     return f"Our POST counter is: {request_counters['POST']}, and our GET counter is: {request_counters['GET']}.\n"
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
